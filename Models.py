136c4fe1b06a8dcca1bc40d9196c799f84715802
a6888b <ayoubbrahim68@gmail.com>
2022-02-24 16:41:24 +0100

    modif sur la methode get_password

--- Models.py
+++ Models.py
@@ -74,8 +74,7 @@ class ModelPassword(ModelMain):
         self.init_db()
 
     def get_password(self):
-        self.curs.execute(f'SELECT password FROM {self.table}')
-        return self.curs.fetchone()
+        return self.curs.execute(f'SELECT password FROM {self.table}').fetchone()
 
     def create_database(self):
         self.curs.execute(f""" 
@@ -85,6 +84,7 @@ class ModelPassword(ModelMain):
         self.conn.commit()
 
     def set_password(self, password_entry: str):
-        self.curs.execute(
-            f"""INSERT INTO {self.table} VALUES("{password_entry}")""")
+        self.curs.execute(f"""INSERT INTO {self.table} 
+                            VALUES("{password_entry}")
+                            """)
         self.conn.commit()
