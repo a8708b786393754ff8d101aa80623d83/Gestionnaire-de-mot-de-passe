5951720b1171a55a2a5633d0706d756dabc3d3b7
a6888b <ayoubbrahim68@gmail.com>
2023-01-13 23:25:59 +0100

    re ecriture code add command_prompt fonction

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 #!/usr/bin/python3
 from controller.controller_base import ControllerBase
+from controller.controller_password import ControllerPassword
+from controller.controller_credentials import ControllerCredentials
+
 
 from model.model_password import ModelPassword
 from model.model_credentials import ModelCredentials
 
 from view.view_base import View
 
-c = ControllerBase(View, ModelCredentials, ModelPassword)
-c.view.password_entry()  # demande du mot de passe
-first_time_connected = c.first_time()
 
-if not first_time_connected: # regarde si c'est la premiere fois qu'il se connecte
-    if c.verif_password():
-        c.set_command()
+def command_prompt():
+    """Interaction with user inputs"""
 
+    while True:
+        cmd = input('>> ')
+        if 'set' in cmd:
+            cmd_split = cmd.split(' ')
+            if len(cmd_split) == 4:
+                controller_credentials.credentials['service'] = cmd_split[1]
+                controller_credentials.credentials['user'] = cmd_split[2]
+                controller_credentials.credentials['password'] = cmd_split[3]
+
+                controller_credentials.set_credentials()
+                print('Les données sont insérer')  # TODO ajoutée une vue
+            else:
+                controller_base.view.help()
+        elif cmd == 'show':
+            controller_credentials.show()
+
+        elif cmd in ['exit', 'q', 'quit']:
+            break
+
+        else:
+            controller_base.view.help()
+
+
+controller_base = ControllerBase(View)
+controller_credentials = ControllerCredentials(View, ModelCredentials)
+controller_password = ControllerPassword(View, ModelPassword)
+
+controller_password.view.password_entry()  # demande du mot de passe
+first_time_connected = controller_password.first_time()
+
+if not first_time_connected:  # regarde si c'est la premiere fois qu'il se connecte
+    if controller_password.verif_password():
+        command_prompt()
     else:
         print('Veuillez verifier mot de passe maitre ')
